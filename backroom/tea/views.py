600b631295c84f6fc069c3aa72e39279bd869675
x-lsx <sagievartur@mail.ru>
2024-04-11 10:13:08 +0500

    бекэнд фильтр

--- backroom/tea/views.py
+++ backroom/tea/views.py
@@ -2,20 +2,51 @@ from django.shortcuts import render
 from tea.models import prepod, stud, faa, group, teoria
 # Create your views here.
 from rest_framework import routers, serializers, viewsets
-
-
+from rest_framework import generics
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+import django_filters
 #----
 class PrepodSerializer(serializers.ModelSerializer):
     class Meta:
         model = prepod
         fields = "__all__"
+        # fields = []
 
 
 class PrepodViewSet(viewsets.ModelViewSet):
     queryset = prepod.objects.all()
     serializer_class = PrepodSerializer
 
-
+class PrepodList(generics.ListAPIView):
+    serializer_class = PrepodSerializer
+    def get_queryset(self):
+        queryset = prepod.objects.all()
+        name = self.request.query_params.get('name')
+        if name is not None:
+            queryset = queryset.filter(prepod__name = name)
+        return queryset
+    
+class PrepodList(generics.ListAPIView):
+    queryset = prepod.objects.all()
+    serializer_class = PrepodSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['name']
+    
+    
+# class Prr(django_filters.FilterSet):
+#     class Meta:
+#         model = prepod
+#         fields = '__all__'  
+    
+# class PrepFilt(viewsets.ModelViewSet):
+#     queryset = prepod.objects.all()
+#     serializer_class = PrepodSerializer
+#     filter_class = Prr
+#     filter_backends = [DjangoFilterBackend]
+    
+    
+    
 #----
 class StudSerializer(serializers.ModelSerializer):
     class Meta:
