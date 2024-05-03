4916483b79fdca3d2918235468d4859805ca3a8f
x-lsx <sagievartur@mail.ru>
2024-05-03 15:04:02 +0500

    new

--- backroom/backroom/urls.py
+++ backroom/backroom/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path,include,re_path
-from tea.views import PrepodViewSet, StudViewSet, TeoriaViewSet, FaaViewSet, PrepodList, StudList,TeoriaList,FileDownloadView,FileDownload1View
+from tea.views import PrepodViewSet, StudViewSet, TeoriaViewSet, FaaViewSet, PrepodList, StudList,TeoriaList,FileDownloadView,GroupViewSet
 from rest_framework import routers
  
 router = routers.DefaultRouter()
@@ -8,12 +8,14 @@ router.register(r'Prepod', PrepodViewSet)
 router.register(r'Stud', StudViewSet)
 router.register(r'Teoria', TeoriaViewSet)
 router.register(r'Faa', FaaViewSet)
-# router.register(r'files', FileViewSet)
+router.register(r'Gro',GroupViewSet)
+
 urlpatterns = [
-    path('files/<str:filename>/', FileDownload1View.as_view()),
+    
     path('files/<int:pk>/dw/', FileDownloadView.as_view()),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/auth/',include('rest_framework.urls')),
     re_path('api/Prepod(?P<name>.+)/$',PrepodList.as_view()),
     re_path('api/Stud(?P<name>.+)/$',StudList.as_view()),
    re_path('api/Teoria(?P<predmet>.+)/$',TeoriaList.as_view()),
--- backroom/tea/views.py
+++ backroom/tea/views.py
@@ -10,18 +10,19 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.http import HttpResponse
 from rest_framework.decorators import action
+from rest_framework.serializers import FileField, ModelSerializer
+
 #----students
 class StudSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = stud
         fields = "__all__"
-
-class Stud2Serializer(serializers.ModelSerializer):
-    prepods = serializers.PrimaryKeyRelatedField(queryset=prepod.objects.all(), many=True)
-    class Meta:
-        model = stud
-        fields = ["id", "name", "group", "prepods"]
+        
+# class Stud2Serializer(serializers.ModelSerializer):
+#     # prepods = serializers.PrimaryKeyRelatedField(queryset=prepod.objects.all(), many=True)
+#     class Meta:
+#         model = stud
+#         fields = ["id", "name", "group"]
         
 class StudViewSet(viewsets.ModelViewSet):
     queryset = stud.objects.all()
@@ -33,11 +34,9 @@ class StudList(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['mail']   
     
-    
-
 #----prepods
 class PrepodSerializer(serializers.ModelSerializer):
-    studs = Stud2Serializer(many = True, read_only = True)
+    # studs = Stud2Serializer(many = True, read_only = True)
     class Meta:
         model = prepod
         fields = "__all__"
@@ -52,19 +51,17 @@ class PrepodList(generics.ListAPIView):
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['mail']
     
-
 #----
 class TeoriaSerializer(serializers.ModelSerializer):
+    file = FileField()
     class Meta:
         model = teoria
         fields = "__all__"
         
-
 class TeoriaViewSet(viewsets.ModelViewSet):
     queryset = teoria.objects.all()
     serializer_class = TeoriaSerializer
        
-
 class TeoriaList(generics.ListAPIView):
     queryset = teoria.objects.all()
     serializer_class = TeoriaSerializer
@@ -76,54 +73,29 @@ class FileDownloadView(APIView):
     def get(self, request, pk):
         file_model = teoria.objects.get(pk=pk)
         serializer = self.serializer_class(file_model)
-        response = FileResponse(file_model.docfile.read(), as_attachment=True)
-        response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_model.docfile.name)
-
+        response = FileResponse(file_model.file.read(), as_attachment=True)
+        response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_model.file.name)
         return response
-
-class FileDownload1View(APIView):
-    serializer_class = TeoriaSerializer
-
-    def get(self, request, filename):
-        file_model = teoria.objects.get(file__name=filename)
-        serializer = self.serializer_class(file_model)
-
-        # Подготовка ответа для загрузки
-        response = FileResponse(file_model.docfile.read(), as_attachment=True)
-        response['Content-Disposition'] = 'attachment; filename="{}"'.format(file_model.docfile.name)
-
-        return response
+    
 #----
 class FaaSerializer(serializers.ModelSerializer):
     class Meta:
         model = faa
         fields = "__all__"
 
-
 class FaaViewSet(viewsets.ModelViewSet):
     queryset = faa.objects.all()
     serializer_class = FaaSerializer
 
-
 #----
-# class PrepodList(generics.ListAPIView):
-#     serializer_class = PrepodSerializer
-#     def get_queryset(self):
-#         queryset = prepod.objects.all()
-#         name = self.request.query_params.get('name')
-#         if name is not None:
-#             queryset = queryset.filter(prepod__name = name)
-#         return queryset
-    
-# class Prr(django_filters.FilterSet):
-#     class Meta:
-#         model = prepod
-#         fields = '__all__'  
-    
-# class PrepFilt(viewsets.ModelViewSet):
-#     queryset = prepod.objects.all()
-#     serializer_class = PrepodSerializer
-#     filter_class = Prr
-#     filter_backends = [DjangoFilterBackend]
-    
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = group
+        fields = "__all__"
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = group.objects.all()
+    serializer_class = GroupSerializer
+
+
     
